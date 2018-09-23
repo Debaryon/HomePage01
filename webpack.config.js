5130a8e592d5626d116c15ec430f9550e0721a5b
Mats Kihlberg <mats@debaryon.com>
2018-09-23 10:31:02 +0200

    installed and configured build process with webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+const SRC_DIR = path.join(__dirname, './src')
+const OUTPUT_DIR = path.join(__dirname, './public')
+
+module.exports = {
+    entry: {
+        main: path.join(SRC_DIR, './main.ts')
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            include: [SRC_DIR],
+            options: {
+                instance: 'main'
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', 'js']
+    },
+    output: {
+        filename: '[name].js',
+        path: OUTPUT_DIR
+    }
+}
